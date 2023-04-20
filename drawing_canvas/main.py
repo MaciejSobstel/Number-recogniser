712bce415f713d8554e2cb5068ddba7b6168eed2
msobstel <maciej.sobstel@gmail.com>
2023-04-20 19:11:45 +0200

    add a drawing canvas and pyproject.toml

---
+++ drawing_canvas/main.py
@@ -0,0 +1,55 @@
+import pygame
+import numpy as np
+
+pygame.init()
+
+WINDOW_SIZE = (800, 800)
+screen = pygame.display.set_mode(WINDOW_SIZE)
+
+pygame.display.set_caption("Draw a number")
+
+
+def create_image(drawing):
+    image = np.zeros((8, 8), dtype=np.int32)
+
+    for i in range(8):
+        for j in range(8):
+            if drawing[i][j] == 255:
+                image[i][j] = 15
+
+    return image
+
+
+drawing = np.zeros((8, 8), dtype=np.int32)
+
+brush_color = (255, 255, 255)
+
+brush_size = 20
+
+drawing_active = False
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            drawing_active = True
+
+        elif event.type == pygame.MOUSEBUTTONUP:
+            drawing_active = False
+
+        elif event.type == pygame.MOUSEMOTION and drawing_active:
+            mouse_pos = pygame.mouse.get_pos()
+            pygame.draw.circle(screen, brush_color, mouse_pos, brush_size)
+            i = mouse_pos[1] // 100
+            j = mouse_pos[0] // 100
+            drawing[i][j] = 255
+
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RETURN:
+                image = create_image(drawing)
+                print(image)
+    pygame.display.update()
+    pygame.display.flip()
--- number_recogniser/model.py
+++ number_recogniser/model.py
@@ -1,11 +1,18 @@
 import pickle
 import numpy as np
+from number_recogniser import package_root
 
 ex = np.array([0.,  0.,  5., 13.,  9.,  1.,  0.,  0.,  0.,  0., 13., 15., 10., 15.,  5.,  0.,  0.,  3., 15.,  2.,  0., 11.,  8.,  0.,  0.,  4., 12.,  0.,  0.,  8.,  8.,  0.,  0.,  5.,  8.,  0.,  0.,  9.,  8., 0.,  0.,  4., 11.,  0.,  1., 12.,  7.,  0.,  0.,  2., 14.,  5., 10., 12.,  0.,  0.,  0.,  0.,  6., 13., 10.,  0.,  0.,  0.])
 ex = ex.reshape(1, -1)
 
-with open('model.pkl', 'rb') as f:
+with open(package_root/"model.pkl", 'rb') as f:
     svm = pickle.load(f)
 
-predicted = svm.predict(ex)
-print(predicted)
+
+def prediction():
+    predicted = svm.predict(ex)
+    print(predicted)
+
+
+if __name__ == "__main__":
+    prediction()
